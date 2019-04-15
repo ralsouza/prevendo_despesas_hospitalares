a17355b18e71b2a3244c4edce4c26e0c79a39cee
Rafael Lima <ls.rafael@icloud.com>
2019-04-15 19:28:31 -0300

    Define business problem

--- 01_predicting_hospital_expenses.R
+++ 01_predicting_hospital_expenses.R
@@ -1,5 +1,9 @@
 # Machine Learning - Regression
-# Predicting Hospital Expenses
 
 # Work Directory
 setwd('/Users/ls_rafael/Documents/GitHub/predicting_hospital_expenses')
+
+##### Business Problem: Predicting Hospital Expenses ####
+# For this analysis, we will use a simulated data set of hypothetical medical expenses from a set 
+# of patients spread across in four regions in Brazil. 
+# This data set has 1.338 observations and 7 variables.
