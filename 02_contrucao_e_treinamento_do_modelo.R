d0819fa442890b25fc4bc34c5541644dffe743ac
Rafael Lima <ls.rafael@icloud.com>
2019-04-16 23:13:16 -0300

    lm

--- 02_contrucao_e_treinamento_do_modelo.R
+++ 02_contrucao_e_treinamento_do_modelo.R
@@ -3,3 +3,7 @@
 
 # Diretório de trabalho
 setwd('/Users/ls_rafael/Documents/GitHub/prevendo_despesas_hospitalares')
+
+#### PASSO 3 - TREINANDO O MODELO ####
+?lm
+modelo <- lm(gastos ~ idade + filhos + bmi + sexo + fumante + regiao, data = despesas)
