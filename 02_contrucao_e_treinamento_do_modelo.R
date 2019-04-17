814705443d0fb35ea84e9b64f47300cfff196fc1
Rafael Lima <ls.rafael@icloud.com>
2019-04-17 07:41:39 -0300

    Sintaxes de funções lineares

--- 02_contrucao_e_treinamento_do_modelo.R
+++ 02_contrucao_e_treinamento_do_modelo.R
@@ -6,4 +6,12 @@ setwd('/Users/ls_rafael/Documents/GitHub/prevendo_despesas_hospitalares')
 
 #### PASSO 3 - TREINANDO O MODELO ####
 ?lm
+#Sintaxe da regressão linear multipla
+# lm(var alvo ~ var preditora 1 + var preditora n ..., data = dataset)
 modelo <- lm(gastos ~ idade + filhos + bmi + sexo + fumante + regiao, data = despesas)
+
+# Regressão linear simples, com uma variável preditora
+# modelo <- lm(gastos ~ idade, data = despesas)
+
+# Formato simplificado
+modelo <- lm(gastos ~ ., data = despesas)
